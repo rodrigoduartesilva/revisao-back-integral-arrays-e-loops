ec4ec0a46fed0c456479de2d7689a40363e28632
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-10-14 16:25:05 -0300

    Classe 05 - Finalizado

---
+++ classe-05/index.js
@@ -0,0 +1,11 @@
+const original = [1, 4, 12, 21, 53, 88, 112];
+const pares = [];
+
+
+for(let item of original){
+    if(item % 2 === 0){
+        pares.push(item);
+    }
+}
+
+console.log(pares);
